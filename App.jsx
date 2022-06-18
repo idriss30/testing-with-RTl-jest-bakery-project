4742b079daaad66739c7cd4899b7c1062c3f7278
idriss30 <idriss.cissoko@hotmail.com>
2022-06-18 04:14:02 -0400

    added button to increase quantity

--- App.jsx
+++ App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 export const App = () => {
+  const [danishes, setDanishes] = React.useState(0);
+  const clickButton = () => {
+    setDanishes(danishes + 1);
+  };
   return (
     <>
       <div>
         <h1>Inventory Contents</h1>
+        <p>Danish, {danishes}</p>
+        <button onClick={clickButton}>Increment danish</button>
       </div>
     </>
   );
