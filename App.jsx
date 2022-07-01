715e1e6cb8905a0418f200c837945f99150f258f
idriss30 <idriss.cissoko@hotmail.com>
2022-07-01 10:20:35 -0400

    adding useEffect to fetch data from server and ItemList component to render it

--- App.jsx
+++ App.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Form } from "./Form.jsx";
+import { API_ADDR } from "./apiaddress.js";
+import { ItemList } from "./itemList.jsx";
 
 export const App = () => {
+  const [items, setItems] = useState({});
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const fetchItems = async () => {
+      const getItems = await fetch(`${API_ADDR}/inventory/`);
+
+      if (isMounted) {
+        setItems(await getItems.json());
+      }
+    };
+    fetchItems();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
   return (
     <>
       <div>
         <h1>Inventory Content</h1>
         <h2>Add item through the Form</h2>
         <Form />
+        <ItemList items={items} />
       </div>
     </>
   );
