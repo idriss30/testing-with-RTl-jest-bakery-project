6dfef73adfdca83931c0462b6456003fad337c8e
idriss30 <idriss.cissoko@hotmail.com>
2022-07-01 10:18:01 -0400

    modified object to represent backend

--- ItemList.test.jsx
+++ ItemList.test.jsx
@@ -4,11 +4,11 @@ import { ItemList } from "./ItemList.jsx";
 
 describe("testing ItemsListComponents", () => {
   test("render list of items with ItemList", () => {
-    const items = {
-      cheesecake: 3,
-      danish: 5,
-      croissant: 20,
-    };
+    const items = [
+      { productName: "cheesecake", productQty: 3 },
+      { productName: "danish", productQty: 5 },
+      { productName: "croissant", productQty: 20 },
+    ];
     const { getByText } = render(<ItemList items={items} />);
     const nodeList = document.querySelector("ul");
     expect(nodeList.childElementCount).toBe(3);
