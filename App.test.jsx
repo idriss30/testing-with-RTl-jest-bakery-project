9e761f3eb89ba23af7dd2b7797749a8d5dbae42e
idriss30 <idriss.cissoko@hotmail.com>
2022-07-02 19:33:16 -0400

    test listItems update and app rendering

--- App.test.jsx
+++ App.test.jsx
@@ -1,8 +1,42 @@
-import { render, fireEvent } from "@testing-library/react";
 import React from "react";
 import { App } from "./App.jsx";
+import nock from "nock";
+import { API_ADDR } from "./apiaddress.js";
 
-test("checking if proper header is displayed", () => {
+import { act, render, waitFor } from "@testing-library/react";
+
+beforeEach(() => {
+  nock(API_ADDR)
+    .get("/inventory/")
+    .reply(200, [
+      { productName: "cheesecake", productQty: 2 },
+      { productName: "croissant", productQty: 5 },
+      { productName: "macaroon", productQty: 96 },
+    ]);
+});
+
+afterEach(() => {
+  waitFor(() => {
+    if (!nock.isDone()) {
+      nock.cleanAll();
+      throw new Error("inventory route not reached");
+    }
+  });
+});
+
+test("renders the appropriate header", async () => {
+  const { findByText } = render(<App />);
+  expect(await findByText("Inventory Content")).toBeInTheDocument();
+});
+
+test("rendering the server's list of items", async () => {
   const { getByText } = render(<App />);
-  expect(getByText("Inventory Content")).toBeInTheDocument();
+
+  const listElement = document.querySelector("ul");
+  waitFor(() => {
+    expect(listElement.childElementCount).toBe(3);
+    expect(getByText("cheesecake - Quantity : 2")).toBeInTheDocument();
+    expect(getByText("croissant - Quantity : 5")).toBeInTheDocument();
+    expect(getByText("macaroon - Quantity : 96")).toBeInTheDocument();
+  });
 });
