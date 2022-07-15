a4167414355c6957edaf52f07741b16d4557faa8
idriss30 <idriss.cissoko@hotmail.com>
2022-07-15 13:41:15 -0400

    added integration test for App

--- App.test.jsx
+++ App.test.jsx
@@ -3,7 +3,8 @@ import { App } from "./App.jsx";
 import nock from "nock";
 import { API_ADDR } from "./apiaddress.js";
 
-import { render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+
 beforeEach(() => {
   nock(API_ADDR)
     .get("/inventory/")
@@ -38,6 +39,50 @@ test("rendering the server's list of items", async () => {
   });
   const ul = document.querySelector("ul");
   expect(ul.children).toHaveLength(3);
+  expect(
+    await screen.findByText("croissant - Quantity : 5")
+  ).toBeInTheDocument();
+  expect(
+    await screen.findByText("macaroon - Quantity : 96")
+  ).toBeInTheDocument();
+});
+
+test("update state with new list items", async () => {
+  render(<App />);
+
+  //mock the request
+  nock(API_ADDR)
+    .post("/inventory/danish")
+    .reply(201, { message: "danish was added to inventory" });
+
+  // get inputs
+  const prodName = screen.getByPlaceholderText("add itemname");
+  const prodQty = screen.getByPlaceholderText("add quantity");
+  const button = screen.getByText("Push item to inventory");
+
+  // wait for  nock first interceptor to intercept the request
+
+  await waitFor(async () => {
+    expect(
+      await screen.findByText("cheesecake - Quantity : 2")
+    ).toBeInTheDocument();
+  });
+
+  // fireEvent
+  fireEvent.change(prodName, { target: { value: "danish" } });
+  fireEvent.change(prodQty, { target: { value: 100 } });
+  fireEvent.click(button);
+
+  // wait for second interceptor Event
+  await waitFor(async () => {
+    expect(
+      await screen.findByText("danish - Quantity : 100")
+    ).toBeInTheDocument();
+  });
+
+  const ul = document.querySelector("ul");
+  expect(ul.childElementCount).toBe(4);
+
   expect(
     await screen.findByText("croissant - Quantity : 5")
   ).toBeInTheDocument();
