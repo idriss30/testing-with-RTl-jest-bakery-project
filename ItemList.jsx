a45c6eb663c21d1370de0ce304d005c2bfbb0f89
idriss30 <idriss.cissoko@hotmail.com>
2022-07-22 11:29:11 -0400

    added component animation with react-spring transition

--- ItemList.jsx
+++ ItemList.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { Transition } from "react-spring/renderprops";
 
 export const ItemList = ({ items }) => {
   const arr = Object.values(items);
 
   return (
     <ul>
-      {arr.map((item, index) => {
-        return (
-          <li key={index}>
-            {generateProductText(item.productName, item.productQty)}
-          </li>
-        );
-      })}
+      <Transition
+        items={arr}
+        initial={null}
+        keys={({ productName }) => productName}
+        from={{ fontSize: 0, opacity: 0 }}
+        enter={{ fontSize: 18, opacity: 1 }}
+        leave={{ fontSize: 0, opacity: 0 }}
+      >
+        {({ productName, productQty }) =>
+          (styleProps) =>
+            (
+              <li key={productName} style={styleProps}>
+                {generateProductText(productName, productQty)}
+              </li>
+            )}
+      </Transition>
     </ul>
   );
 };
