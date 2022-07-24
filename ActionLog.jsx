b162509696358fe8c92dc68893fe62eade4abb56
idriss30 <idriss.cissoko@hotmail.com>
2022-07-24 07:07:20 -0400

    added testid to action-log

--- ActionLog.jsx
+++ ActionLog.jsx
@@ -3,17 +3,19 @@ import React from "react";
 export const ActionLog = ({ actions }) => {
   return (
     <>
-      <h2>Action Log</h2>
-      <ul>
-        {actions.map(({ time, message, data }, i) => {
-          const date = new Date(time).toUTCString();
-          return (
-            <li key={i}>
-              {date} - {message} - {JSON.stringify(data)}
-            </li>
-          );
-        })}
-      </ul>
+      <div data-testid="action-log">
+        <h2>Action Log</h2>
+        <ul>
+          {actions.map(({ time, message, data }, i) => {
+            const date = new Date(time).toUTCString();
+            return (
+              <li key={i}>
+                {date} - {message} - {JSON.stringify(data)}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
