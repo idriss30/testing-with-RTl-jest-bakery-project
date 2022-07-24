d5175ef7dc5f76127706f05770264ce2efe8cbf6
idriss30 <idriss.cissoko@hotmail.com>
2022-07-24 07:08:19 -0400

    removed screen import

--- ActionLog.test.jsx
+++ ActionLog.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { ActionLog } from "./ActionLog.jsx";
 
 test("testing action log functions", () => {
